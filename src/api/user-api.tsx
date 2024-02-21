b3871fb03428aa1950c85e172dda4ee0587888a7
Manish <tamangmanish3456@gmail.com>
2024-02-21 12:59:07 +0545

    add: Auth0Token

--- src/api/user-api.tsx
+++ src/api/user-api.tsx
@@ -1,35 +1,45 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+import { useMutation } from "react-query";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 type CreateUserRequest = {
-    auth0Id: string;
-    email: string;
-    name?: string;
-    picture?: string;
-}
-
-import { useMutation } from "react-query";
+  auth0Id: string;
+  email: string;
+  name?: string;
+  picture?: string;
+};
 
 export const useCreateUser = () => {
-    const createUser = async(userData : CreateUserRequest) => {
-        const response = await fetch(`${API_BASE_URL}/api/user`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(userData)
-        });
+  const { getAccessTokenSilently } = useAuth0();
 
-        if(!response.ok){
-            throw new Error("Failed to create user")
-        };
+  const createUser = async (userData: CreateUserRequest) => {
+    const accessToken = await getAccessTokenSilently();
+    const response = await fetch(`${API_BASE_URL}/api/user`, {
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to create user");
     }
+  };
 
-    const { mutateAsync: registerUser, isLoading, isError, isSuccess } = useMutation(createUser);
+  const {
+    mutateAsync: registerUser,
+    isLoading,
+    isError,
+    isSuccess,
+  } = useMutation(createUser);
 
-    return {
-        registerUser,
-        isLoading,
-        isError,
-        isSuccess
-    }
-}
+  return {
+    registerUser,
+    isLoading,
+    isError,
+    isSuccess,
+  };
+};
