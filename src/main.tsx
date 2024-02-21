2414ec89b60f4bb1a550d023d0ca30595a748f16
Manish <tamangmanish3456@gmail.com>
2024-02-21 11:53:25 +0545

    react-query setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,30 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import "./global.css";
-
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+
 import AppRoutes from "./AppRoutes.tsx";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import Auth0ProviderWithNavigate from "./auth/auth0ProviderWithNavigate.tsx";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="eatsy-theme">
       <Router>
-        <Auth0ProviderWithNavigate>
-          <AppRoutes />
-        </Auth0ProviderWithNavigate>
+        <QueryClientProvider client={queryClient}>
+          <Auth0ProviderWithNavigate>
+            <AppRoutes />
+          </Auth0ProviderWithNavigate>
+        </QueryClientProvider>
       </Router>
     </ThemeProvider>
   </React.StrictMode>
